a8aca5a75456783f0f8ee05c6e3f8adaaf062455
Will Jones <wsjones1101@gmail.com>
2018-08-12 17:00:07 +0200

    Implementation of data features

---
+++ app/models/concerns/placeholder.rb
@@ -0,0 +1,9 @@
+# Loads in helper modules
+
+module Placeholder
+	extend ActiveSupport::Concern
+
+	def self.image_generator(height:, width:)
+		"http://via.placeholder.com/#{height}x#{width}"	
+	end
+end
