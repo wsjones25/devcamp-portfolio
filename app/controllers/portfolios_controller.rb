383bb529bfe1ef3495000d4430d07c955342f62e
Will Jones <wsjones1101@gmail.com>
2018-08-12 07:44:41 +0200

    Implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,13 +3,16 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+	def angular
+		@angular_portfolio_items = Portfolio.angular
+	end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
 
 	def create
     	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
 	    respond_to do |format|
 	    	if @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 	        	format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
@@ -25,7 +28,6 @@ class PortfoliosController < ApplicationController
 
     def update
     	@portfolio_item = Portfolio.find(params[:id])
-
 	    respond_to do |format|
 	      	if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
 	        	format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
@@ -41,7 +43,6 @@ class PortfoliosController < ApplicationController
 
   	def destroy
   		@portfolio_item = Portfolio.find(params[:id])
-
     	@portfolio_item.destroy
     	respond_to do |format|
       		format.html { redirect_to portfolios_url, notice: 'Portfolio was removed.' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to:'portfolios#show', as: 'portfolio_show'
+
   get 'about', to:'pages#about'
   get 'contact', to:'pages#about'
 
